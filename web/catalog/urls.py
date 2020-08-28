fc7b23b9be8e82eb8357af736b1dafa5f8606eda
germain1909 <56738516+germain1909@users.noreply.github.com>
2020-08-28 09:16:46 -0400

    add 1

--- web/catalog/urls.py
+++ web/catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int:1>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/1', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
     path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
